74129d296362674289e2d1600b3d625f38a74d4c
IvanLotoPascalCase <ivan.olivares@outlook.com>
2023-02-05 20:55:43 -0300

    Fixing secciones

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
     return (
 
 
-        <div className='flex justify-center bg-primary-alt top-0 relative gap-10 bg-hero-background bg-cover h-[1000px]'>
-            <div className='flex items-center justify-center flex-col text-center pb-4 mt-10' >
+        <div className='flex justify-center bg-primary-alt top-0 relative gap-10 bg-hero-background bg-cover h-[1100px]'>
+            <div className='flex items-center justify-start flex-col text-center pb-4 mt-10' >
                 <h1 className='text-8xl font-bold font-pf-dp text-f-primary pt-10'>
                     Iván Olivares
                 </h1>
@@ -14,7 +14,7 @@ const Hero = () => {
                     "loto"
                 </h4>
                 <p className='text-2xl font-semibold italic text-f-secondary pb-2'>Front-End Developer and Software Engineer.</p>
-                <p className='text-2xl font-normal italic text-f-secondary pb-2'>Chilean Developer, 24 years old from Valparaiso.</p>
+                <p className='text-2xl font-normal italic text-f-secondary pb-2'>Chilean Developer, 24 years old from Valparaíso.</p>
             </div>
         </div>
     )
--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Skills = () => {
     return (
         <div className='bg-primary-skill h-[1200px]'>
-            <div className='flex items-center justify-center pt-10'>
+            <div className='flex items-center justify-center pt-20'>
                 <h1 className='text-6xl font-bold font-pf-dp text-f-primary'>
                     Skills
                 </h1>
