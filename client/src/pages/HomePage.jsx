3fb073db4b4821e9b6eaf545dac34b2546dac42e
IvanLotoPascalCase <ivan.olivares@outlook.com>
2023-02-06 18:47:40 -0300

    Sección About Me agregada

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import NavBar from "../components/NavBar.jsx";
 import Hero from "../components/Hero.jsx";
 import Skills from "../components/Skills.jsx";
+import AboutMe from "../components/AboutMe.jsx";
 
 const HomePage = () => {
     return (
         <div>
             <NavBar></NavBar>
             <Hero></Hero>
+            <AboutMe></AboutMe>
             <Skills></Skills>
         </div>
     )
